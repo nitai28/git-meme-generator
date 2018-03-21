661ad243b4997ab47140c6960f5ee3c6bdf24ff2
Nitai Shavit <nitai230@gmail.com>
2018-03-21 13:09:36 +0200

    Fixed the canvas and added input text to canvas

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ var gMeme = {
         line: 'I never eat Falafel',
         size: 20,
         align: 'left',
-        color: 'red'
     }]
 }
+var gCurrImg;
 
 function init() {
     gNextId = 1;
@@ -23,12 +23,12 @@ function init() {
 
 
 function renderGallery(imgs) {
-    var elGrid = document.querySelector('.gallery-grid');
+    var elGrid = document.querySelector('.gallery-grid'); var strHtml = '';
     var strHtml = '';
     imgs.forEach(function (img, idx) {
         strHtml += `<div 
         style="background-image:url(${img.url})" 
-        data-id="${img.id}" onclick="openGallery()" class="meme-pic"
+        data-id="${img.id}" onclick="chooseImg(${img.id}); openGallery();" class="meme-pic"
         ></div>
         `
     });
@@ -46,23 +46,68 @@ function addImg(url, keywords) {
     gImgs.push(img);
     renderGallery(gImgs);
 }
-
-function openGallery(){
-    var elpage1=document.querySelectorAll('.page1');
-    var elpage2=document.querySelector('.page2');
-    elpage2.classList.toggle('hide');
-    elpage1.forEach(function(elpage){
-        elpage.classList.toggle('hide');
-    });
-}   
 function filterImages(event) {
     var elSearch = document.querySelector('#search');
     var keyword = elSearch.value;
-    if(!keyword) return renderGallery(gImgs);
-    var imgs = gImgs.filter(function(img) {
+    if (!keyword) return renderGallery(gImgs);
+    var imgs = gImgs.filter(function (img) {
         return img.keywords.some(function (imgKeyword) {
             return imgKeyword.indexOf(keyword) !== -1;
         })
     });
     renderGallery(imgs);
+}
+
+function openGallery() {
+    var elpage1 = document.querySelectorAll('.page1');
+    var elpage2 = document.querySelector('.page2');
+    elpage2.classList.toggle('hide');
+    elpage1.forEach(function (elpage) {
+        elpage.classList.toggle('hide');
+    });
+    var pos = getPosImg(gImgs);
+    drawImg(gImgs[pos]);
+}
+
+
+function drawImg(img) {
+    if (!img) {
+        var pos = gImgs.findIndex(function (img) {
+            return img.id === gMeme.selectedImgId
+        })
+        img = gImgs[pos];
+    }
+    var canvas = document.getElementById("my-canvas"),
+        ctx = canvas.getContext("2d");
+
+    var background = new Image();
+    background.src = img.url;
+    console.log(background.height)
+    canvas.height = background.height;
+    canvas.width = background.width;
+    // Make sure the image is loaded first otherwise nothing will draw.
+    background.onload = function () {
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        addText();
+    }
+}
+
+function addText() {
+    var elInput = document.querySelector('.text-pic');
+    var canvas = document.getElementById('my-canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.font = "48px serif";
+    ctx.fillStyle = '#fff';
+    ctx.fillText(elInput.value, 50, 100);
+}
+
+function chooseImg(id) {
+    console.log(id);
+    gMeme['selectedImgId'] = id;
+}
+
+function getPosImg(gImgs){
+    return gImgs.findIndex(function (img) {
+        return img.id === gMeme.selectedImgId
+    })
 }
